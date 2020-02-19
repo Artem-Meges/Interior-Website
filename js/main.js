047a24be826eb61fc0bf964f52203f6a98ebae88
Artem <artemmeges@gmail.com>
2020-02-19 13:27:51 +0200

    add header

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('.header__search-icon').on('click', function(){
+        $('.header__search-bar').slideToggle();
+    });
+
+    $('.header-slider').slick({
+        
+    });
+
+});
