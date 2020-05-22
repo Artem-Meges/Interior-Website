f5309408ca6107dd387d05c713e6c8b219da8c58
Artem Meges <artemmeges@gmail.com>
2020-05-22 19:43:59 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -4,11 +4,37 @@ $(function(){
         $('.header__search-bar').slideToggle();
     });
 
-    $('.header-slider').slick({       
+    $('.header-slider').slick({
+        responsive: [
+            {
+                breakpoint: 1500,
+                    settings: {
+                        arrows: false,
+                        dots: true,                 
+                    }
+            }
+        ]     
     });
 
     $('.project__slider').slick({
-        speed: 1000       
+        speed: 1000,
+        responsive: [
+            {
+                breakpoint: 1500,
+                    settings: {
+                        arrows: false,
+                        dots: true,                 
+                    }            
+            },
+            // {
+            //     breakpoint:  1310,
+            //         settings: "unslick"
+            // }
+        ]            
+    });
+
+    $('.header__menu-btn').on('click', function() {
+        $('.header__menu').slideToggle();
     });
 
 });
